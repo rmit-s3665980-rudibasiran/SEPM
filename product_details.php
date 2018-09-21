3458a39bc4e71f932dcf40dc1410ad74cffdd152
rudibasiran <s3665980@student.rmit.edu.au>
2018-09-21 23:57:01 +1000

    Product Details

--- product_details.php
+++ product_details.php
@@ -1,5 +1,3 @@
-
-
 <?php
 setlocale(LC_MONETARY,"en_AU");
 error_reporting(0);
@@ -13,32 +11,31 @@ include 'inc/header.php';
 $loginType = stripInput($_SESSION["loginType"]); 
 $pCode = stripInput($_SESSION["pCode"]); 
 
-
 ?>
 
 <style>
-table {
-    font-family: arial, sans-serif;
+#products {
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
     border-collapse: collapse;
-    width: 100%;
+    font-size: 12px;
+    width: 90%;
 }
 
-td, th {
-    border: 1px solid #dddddd;
-    text-align: left;
+#products td, #products th {
+    border: 1px solid #ddd;
     padding: 8px;
 }
 
-tr:nth-child(even) {
-    background-color: #f2f2f2;
-    font-family: arial;
-    font-size: 12px;
-}
+#products tr:nth-child(even){background-color: #f2f2f2;}
 
-tr:nth-child(odd) {
-    background-color: #FFFFFF;
-    font-family: arial;
-    font-size: 12px;
+#products tr:hover {background-color: #ddd;}
+
+#products th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: black;
+    color: white;
 }
 
 img {
@@ -53,7 +50,7 @@ img:hover {
 }
 
 .text {
-    font-size: 15px;
+    font-size: 12px;
 }
 
 .dollar:before {
@@ -65,27 +62,27 @@ img:hover {
 </style>
 
 
+<br><br><br>
 
-<h5>Product Chosen</h5>
-
-<table>
-  <tr>
-    <th>Category</th>
-    <th>Code</th>
-    <th>Name</th>
-    <th>Image</th>
-    <th>Description</th>
-    <th>Price</th>
-    <th>Add</th>
-    <th>Quantity</th>
-    <th>Remove</th>
-  </tr>
+<table id="products" align="center">
+    <tr>
+        <th>Category</th>
+        <th>Code</th>
+        <th>Name</th>
+        <th>Image</th>
+        <th>Description</th>
+        <th>Price</th>
+        <th>Add</th>
+        <th>Quantity</th>
+        <th>Remove</th>
+    </tr>
 
 
 
 <?php
 $str = $category = $code = $name = $image = $desc = $price = "";
 
+$overallCategory = "";
 $myfile = fopen("product.txt", "r") or die("Unable to open file!");
 
 // Output one line until end-of-file for selected items
@@ -97,6 +94,7 @@ while(!feof($myfile)) {
       list($category, $code, $brand, $name, $image, $desc, $price) = explode(";", $str.";;;;;");
       if ($name <> "") {
         if ($code == $pCode) {
+            $overallCategory = $category;
             $image_path = 'images/';
             echo '<div class="text">';
             echo '<tr>';
@@ -113,13 +111,54 @@ while(!feof($myfile)) {
             echo '</div>';
         }
 
-      }
-      
+      } 
     }
-
-   
 }
-  fclose($myfile);
+?>
+
+
+    <tr>
+        <th colspan="9">Other Suggestions in the same Category</th>
+    </tr>
+
+<?php
+$str = $category = $code = $name = $image = $desc = $price = "";
+
+fclose($myfile);
+
+$myfile = fopen("product.txt", "r") or die("Unable to open file!");
+
+// Output one line until end-of-file for selected items
+while(!feof($myfile)) {
+
+    $str = "";
+  	$str = fgets($myfile);
+    if (substr($str, 0, 1) <> "#")  {
+      list($category, $code, $brand, $name, $image, $desc, $price) = explode(";", $str.";;;;;");
+      if ($name <> "") {
+        if ($overallCategory == $category && $pCode <> $code) {
+            $image_path = 'images/';
+            echo '<div class="text">';
+            echo '<tr>';
+            echo '<td>' . $category   . '</td>';
+            echo '<td>' . $code       . '</td>';
+            echo '<td>' . $name       . '</td>';
+            echo '<td><a target="_blank" href="' .$image_path.$image .'"><img src="'. $image_path.$image.'" alt="' .$name.'" style="width:150px"></td>';
+            echo '<td>'               . $desc       . '</td>';
+            echo '<td align="right">' . money_format('%i',$price)      . '</td>';
+            echo '<td> Add Button </td>';
+            echo '<td> Quantity </td>';
+            echo '<td> Remove Button </td>';
+            echo '</tr>';
+            echo '</div>';
+        }
+
+      } 
+    }
+}
+fclose($myfile);
+
+
 ?>
 </table>
 
